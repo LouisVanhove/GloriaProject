72192ab20415cb55931e3028acc9e2684b74589a
Louis Vanhove <louis.vanhove0@gmail.com>
2017-07-31 11:31:17 +0200

    pouet

---
+++ src/fr/eni/gloria/bean/TestASupprimer.java
@@ -0,0 +1,15 @@
+/**
+ * @author lvanhove2017
+ * @date 31 juil. 2017
+ * @version GloriaProject V1.0
+ */
+package fr.eni.gloria.bean;
+
+/**
+ * @author lvanhove2017
+ * @date 31 juil. 2017
+ * @version GloriaProject V1.0
+ */
+public class TestASupprimer {
+
+}
